67c44d2a9af38134d75b4c58ef71be873474cdc3
Fam <fam.ghaly@ucalgary.ca>
2024-02-17 22:21:27 -0700

    updated index to attempt markers

--- TouchGrass/TouchGrassApp/views.py
+++ TouchGrass/TouchGrassApp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Facility
+
 
 def index(request):
     return render(request, 'TouchGrassApp/index.html')
+
+def map_view(request):
+    facilities = Facility.objects.all()  # Query all facilities from your database
+    context = {
+        'facilities': facilities
+    }
+    return render(request, 'TouchGrassApp/index.html', context)
